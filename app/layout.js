07b9bbb114465a55968cea0bbdd2d797dba597d7
harishamsakutty-hash <haris.hamsakutty@gmail.com>
2026-05-05 12:49:05 -0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Ependy-Match AI',
+  description: 'Molecular Tumor Board for Ependymoma',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
